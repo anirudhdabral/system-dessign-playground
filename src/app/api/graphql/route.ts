46a46686ae25cc71ca3b5b4937818ae9f3d05da6
Anirudh Dabral <anirudhdabral49@gmail.com>
2026-03-04 10:01:32 +0530

    fixed route issue

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { NextRequest } from "next/server";
 import { typeDefs } from "@/lib/graphql/schema";
 import { resolvers } from "@/lib/graphql/resolvers";
 import { getServerSession } from "next-auth";
@@ -10,11 +11,17 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const handler = startServerAndCreateNextHandler(server, {
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
   context: async (req) => {
     const session = await getServerSession(authOptions);
     return { session };
   },
 });
 
-export { handler as GET, handler as POST };
+export async function GET(req: NextRequest): Promise<Response> {
+  return handler(req);
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return handler(req);
+}
